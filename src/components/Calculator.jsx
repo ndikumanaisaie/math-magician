bc88d571aa5f81166c8ca49ae2b3d01e242e189a
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-10-26 08:15:43 +0100

    Fix css problems and correct disabled linters

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,20 +1,8 @@
 /* eslint-disable no-nested-ternary */
-/* eslint-disable react/button-has-type */
-/* eslint-disable react/no-unused-state */
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 
 class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sign: '',
-      num: 0,
-      res: 0,
-    };
-  }
-
   render() {
     const btnValues = [
       ['AC', '+-', '%', '÷'],
@@ -29,15 +17,16 @@ class Calculator extends Component {
         <div className="screen">0</div>
         <div className="button-box">
           {
-            btnValues.flat().map((btn, i) => (
+            btnValues.flat().map((btn) => (
               <button
                 key={btn}
+                type="button"
                 className={
                   btn === '=' || btn === '÷' || btn === 'x' || btn === '-' || btn === '+'
                     ? 'sign-btn'
                     : btn === 0
                       ? 'zero'
-                      : 'keypad'
+                      : '0'
                 }
                 value={btn}
               >
