9b813f51d3e7f7527dcc3a5d5c65e28ca8b799ff
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-02 10:14:55 +0100

    Change styles to match wireframe

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => (
     <h2>Math Magician</h2>
     <div className="nav">
       <Link to="/">Home</Link>
+      <span className="vertical-bar" />
       <Link to="calculator">Calculator</Link>
+      <span className="vertical-bar" />
       <Link to="quotes">Quotes</Link>
     </div>
   </div>
